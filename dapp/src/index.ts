ac8b21217c556c12e4f9733e9114eb3afdffd60c
jjhbk <jp@ph.iitr.ac.in>
2024-08-06 17:09:51 +0530

    initial commit

---
+++ dapp/src/index.ts
@@ -0,0 +1,56 @@
+import { createApp } from "@deroll/app";
+import { createRouter } from "@deroll/router";
+import { createERC20TransferVoucher, createWallet } from "@deroll/wallet";
+import { decodeFunctionData, parseAbi } from "viem";
+
+// create app
+const app = createApp({ url: "http://127.0.0.1:8080/host-runner" });
+
+// create wallet
+const wallet = createWallet();
+
+const router = createRouter({ app });
+router.add<{ address: string }>("wallet/:address", ({ params: { address } }) =>
+    JSON.stringify(wallet.getWallet(address), (_, v) =>
+        typeof v === "bigint" ? v.toString() : v,
+    ),
+);
+
+
+app.addInspectHandler(router.handler);
+
+app.addAdvanceHandler(wallet.handler);
+// define application ABI
+const abi = parseAbi(["function withdraw(address token, uint256 amount)"]);
+
+// handle input encoded as ABI function call
+app.addAdvanceHandler(async ({ metadata, payload }) => {
+    const { functionName, args } = decodeFunctionData({ abi, data: payload });
+
+    switch (functionName) {
+        case "withdraw":
+            const [token, amount] = args;
+            const recipient = metadata.msg_sender;
+
+            // encode voucher of token transfer to requester
+            const voucher = createERC20TransferVoucher(
+                token,
+                recipient,
+                amount,
+            );
+
+            // create voucher output
+            await app.createVoucher(voucher);
+            return "accept";
+    }
+});
+// log incoming advance request
+app.addAdvanceHandler(async ({ payload }) => {
+    await app.createNotice({ payload }); //just for logging purposes
+    await app.createReport({ payload }); //just for logging purposes
+    return "accept";
+});
+
+
+// start app
+app.start().catch((e) => process.exit(1));
---
+++ frontend/src/App.tsx
@@ -0,0 +1,39 @@
+import {
+  decodeFunctionData,
+  encodeFunctionData,
+  getAddress,
+  parseAbi,
+} from "viem";
+import Account from "./components/Account";
+import Notices from "./components/Notices";
+import Reports from "./components/Reports";
+import SendERC20 from "./components/SendERC20";
+import SendERC721 from "./components/SendERC721";
+import SendEther from "./components/SendEther";
+import SimpleInput from "./components/SimpleInput";
+import Vouchers from "./components/Vouchers";
+
+function App() {
+  const abi = parseAbi(["function withdraw(address token, uint256 amount)"]);
+  const input = encodeFunctionData({
+    abi: abi,
+    functionName: "withdraw",
+    args: [getAddress("0x92C6bcA388E99d6B304f1Af3c3Cd749Ff0b591e2"), BigInt(2)],
+  });
+  const { functionName, args } = decodeFunctionData({ abi, data: input });
+
+  console.log("encoded", input, functionName, args);
+  return (
+    <>
+      <Account />
+      <SimpleInput />
+      <SendEther />
+      <SendERC20 />
+      <SendERC721 />
+      <Reports />
+      <Vouchers />
+    </>
+  );
+}
+
+export default App;
